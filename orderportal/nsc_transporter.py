624854cfc82c5a04073ad4447d6d6c0e3fe595d3
fa2k <pmb@fa2k.net>
2016-06-29 21:31:48 +0200

    This is a crap solution. To be reworked next week....

--- orderportal/nsc_transporter.py
+++ orderportal/nsc_transporter.py
@@ -1,20 +1,30 @@
 import itertools
 
+def any_val(v):
+    return v
+
+def int_val(v):
+    return i
+
+def str_val(v):
+    return ""
+
+
 # First element: Internal label
 # Second element: Header in Excel sheet
 SAMPLE_FIELDS = [
-        ("sample_number",       "Number"),
-        ("plate",               "Plate"),
-        ("sample_name",         "Sample name"),
-        ("conc",                "Conc."),
-        ("a_260_280",           "A260/280"),
-        ("a_260_230",           "A260/230"),
-        ("volume",              "Volume provided"),
-        ("total_dna_rna",       "Total DNA / RNA"),
-        ("index_name",          "Index name"),
-        ("index_seq",           "Index Seq"),
-        ("primers",             "Primers, Linkers or RE"),
-        ("num_reads",            "Approx no. Reads")
+        ("sample_number",       "Number",           int_req),
+        ("plate",               "Plate",            any_val),
+        ("sample_name",         "Sample name",      str_req),
+        ("conc",                "Conc.",            float_req),
+        ("a_260_280",           "A260/280",         float_opt),
+        ("a_260_230",           "A260/230",         float_opt),
+        ("volume",              "Volume provided",  float_opt),
+        ("total_dna_rna",       "Total DNA / RNA",  float_opt),
+        ("index_name",          "Index name",       str_opt),
+        ("index_seq",           "Index Seq",        str_opt),
+        ("primers",             "Primers, Linkers or RE", str_opt),
+        ("num_reads",            "Approx no. Reads", str_opt)
         ]
 
 
@@ -23,6 +33,13 @@ class ImportException(Exception):
 
 
 def import_file(fh):
+    """Simple importer which reads the sample table Excel file and
+    produces a list of dicts, one for each sample (identical to the
+    format stored in the DB and exported to the LIMS, etc.)
+    
+    Only minimal validation is done. On error, an ImportException 
+    is raised, with a message indicating the cause.
+    """
     try:
         wb = load_workbook(fh)
     except Exception as e:
@@ -39,17 +56,64 @@ def import_file(fh):
     for i in range(20):
         val = ws.cell(row=1, column=i).value
         if val:
-            for key, label in SAMPLE_FIELDS:
+            for key, label, _ in SAMPLE_FIELDS:
                 if str(val).lower().startswith(label.lower()):
                     col_of[key] = i
 
     MAX_SAMP = 16000
-    for i in range(MAX_SAMP):
-        pass
-
+    samples = []
+    for i in range(3, MAX_SAMP):
+        name = ws.cell(row=i, column=col_of['sample_name'])
+        if name:
+            sample = {}
+            for key, _, _ in SAMPLE_FIELDS:
+                sample[key] = ws.cell(row=i, column=col_of[key]).value
+        else:
+            break
+        
     if ws.cell(row=i+1, column=1).value:
         raise ImportException("We only support importing {0} samples. Please "+
                 "contact NSC staff if you would like to submit more samples, "+
                 "and the webmaster "+
                 "may be able to increase this limit (MAX_SAMP).".format(MAX_SAMP))
-    
+
+    return samples
+
+
+def validate_table(samples_raw):
+    """Main sample table validation.
+
+    Return value:
+        If all fields are valid, it returns
+            (table, None)
+        The table includes the converted values of each cell, 
+        according to the conversion functions defined in the
+        SAMPLE_FIELDS array.
+
+        If there are any validation errors, it returns a list
+        notes as the second element of the tuple:
+            (table, notes)
+        Any validated value has a corresponding entry in table.
+        However, if an entry does not pass validation, it 
+        instead has a tuple in notes, containing the raw 
+        (unconverted) value and a validation message.
+
+    """
+
+
+    notes = []
+    samples = []
+    for sample_raw in samples_raw:
+        note = {}
+        sample = {}
+        for key,_,fn in SAMPLE_FIELDS:
+            try:
+                sample[k] = fn(sample_raw[key])
+            except ValidationError, e:
+                note[k] = (sample_raw[k], e)
+
+
+
+
+
+
